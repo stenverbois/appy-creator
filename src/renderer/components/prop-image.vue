ea8be05ed7351de798911ec0837218d2edada35c
thierryderuyttere <thierryderuyttere@hotmail.com>
2016-02-15 15:14:41 +0100

    aesthetics

---
+++ src/renderer/components/prop-image.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="col s12">
+
+  </div>
+</template>
+
+<script lang="coffee">
+module.exports =
+  props: ['data']
+</script>
--- src/renderer/views/components.vue
+++ src/renderer/views/components.vue
@@ -8,6 +8,11 @@
   overflow: hidden;
   height: ~"calc(100% - @{top-bar-height} - @{footer-bar-height})";
 }
+
+.collapsible-header{
+    background-color: lightgray;
+}
+
 </style>
 
 <template>
@@ -16,25 +21,12 @@
       <li>
         <div class="collapsible-header"><i class="material-icons">view_module</i>Standard</div>
         <div class="collapsible-body">
-          <button class="btn"
-                  @click="state.app.addComponent('Button')">
-            Button
-          </button>
-
-          <button class="btn"
-                  @click="state.app.addComponent('Label')">
-            Label
-          </button>
-
-          <button class="btn"
-                  @click="state.app.addComponent('Textbox')">
-            Textbox
-          </button>
-
-          <button class="btn"
-                  @click="state.app.addComponent('Image')">
-            Image
-          </button>
+          <ul class="collection">
+            <li class="collection-item" @click="state.app.addComponent('Button')">Button</li>
+            <li class="collection-item" @click="state.app.addComponent('Label')">Label</li>
+            <li class="collection-item" @click="state.app.addComponent('Textbox')">Textbox</li>
+            <li class="collection-item" @click="state.app.addComponent('Image')">Image</li>
+          </ul>
         </div>
       </li>
       <li>
