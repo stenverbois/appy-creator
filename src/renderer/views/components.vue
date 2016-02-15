16c20604779e0a51d68e2e3f62c3e3813a75a2b9
Sten Verbois <stenverbois@gmail.com>
2016-02-15 18:11:08 +0100

    Component button styling

--- src/renderer/views/components.vue
+++ src/renderer/views/components.vue
@@ -11,6 +11,12 @@
   .collapsible-header {
     background-color: lightgray;
   }
+
+  .collection-item {
+    a.btn-flat {
+      width: 100%;
+    }
+  }
 }
 
 
@@ -23,10 +29,18 @@
         <div class="collapsible-header"><i class="material-icons">view_module</i>Standard</div>
         <div class="collapsible-body">
           <ul class="collection">
-            <li class="collection-item" @click="state.app.addComponent('Button')">Button</li>
-            <li class="collection-item" @click="state.app.addComponent('Label')">Label</li>
-            <li class="collection-item" @click="state.app.addComponent('Textbox')">Textbox</li>
-            <li class="collection-item" @click="state.app.addComponent('Image')">Image</li>
+            <li class="collection-item">
+              <a class="waves-effect waves-red btn-flat" @click="state.app.addComponent('Button')">Button</a>
+            </li>
+            <li class="collection-item">
+              <a class="waves-effect waves-red btn-flat" @click="state.app.addComponent('Label')">Label</a>
+            </li>
+            <li class="collection-item">
+              <a class="waves-effect waves-red btn-flat" @click="state.app.addComponent('Textbox')">Textbox</a>
+            </li>
+            <li class="collection-item">
+              <a class="waves-effect waves-red btn-flat" @click="state.app.addComponent('Image')">Image</a>
+            </li>
           </ul>
         </div>
       </li>
