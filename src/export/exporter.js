91ee79216ca1500dd0631c4229be11b1cd135359
Sten Verbois <stenverbois@gmail.com>
2016-02-17 21:57:02 +0100

    Start parsing json file in exporter.js

--- src/export/exporter.js
+++ src/export/exporter.js
@@ -8,12 +8,18 @@ var imageTemplate = Handlebars.compile(fs.readFileSync("./templates/image.html")
 var labelTemplate = Handlebars.compile(fs.readFileSync("./templates/label.html").toString());
 var textboxTemplate = Handlebars.compile(fs.readFileSync("./templates/textbox.html").toString());
 
+// Read json file
+if(process.argv[2]) {
+    var appDescription = JSON.parse(fs.readFileSync(process.argv[2]).toString())
+}
+else {
+  // Test stuff if we don't want to parse a file
+  /* Example for button
 
-/* Example for button
+  var buttonResult = buttonTemplate({width: 100, height: 100, name: "test", on_click_method: ""});
+  console.log(buttonResult);
+  */
 
-var buttonResult = buttonTemplate({width: 100, height: 100, name: "test", on_click_method: ""});
-console.log(buttonResult);
-*/
-
-var labelResult = labelTemplate({name: "Yay it twerks", visibility: true});
-console.log(labelResult);
+  var labelResult = labelTemplate({name: "Yay it twerks", visibility: true});
+  console.log(labelResult);
+}
