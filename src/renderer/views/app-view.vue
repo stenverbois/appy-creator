95d3440d34e929b5707a3c010e145171e4a2d885
Sten Verbois <stenverbois@gmail.com>
2016-03-04 20:16:03 +0100

    Zero index row and col

--- src/renderer/views/app-view.vue
+++ src/renderer/views/app-view.vue
@@ -102,8 +102,8 @@ module.exports =
       widget = $('.gridster li.selected').first()
       Vue.nextTick =>
         @selectedComponent.properties.dim.value =
-          row: parseInt widget.attr("data-row")
-          col: parseInt widget.attr("data-col")
+          row: parseInt(widget.attr("data-row")) - 1
+          col: parseInt(widget.attr("data-col")) - 1
           width: parseInt widget.attr("data-sizex")
           height: parseInt widget.attr("data-sizey")
 </script>
