9ae9b80234b34b924ec0f60d2476bd91849c7aa1
Sten Verbois <stenverbois@gmail.com>
2016-03-04 19:47:08 +0100

    Rename sizex/sizey to width/height

--- src/renderer/views/app-view.vue
+++ src/renderer/views/app-view.vue
@@ -102,8 +102,8 @@ module.exports =
       widget = $('.gridster li.selected').first()
       Vue.nextTick =>
         @selectedComponent.properties.dim.value =
-          row: parseInt(widget.attr("data-row"))
-          col: parseInt(widget.attr("data-col"))
-          sizex: parseInt(widget.attr("data-sizex"))
-          sizey: parseInt(widget.attr("data-sizey"))
+          row: parseInt widget.attr("data-row")
+          col: parseInt widget.attr("data-col")
+          width: parseInt widget.attr("data-sizex")
+          height: parseInt widget.attr("data-sizey")
 </script>
